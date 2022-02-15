2355496855e134ed733ec55e1a996b32e0925322
Aviad-hub <57433539+Aviad-hub@users.noreply.github.com>
2022-02-15 15:51:32 +0200

    ex 14 done

--- src/exercises/exercise1.js
+++ src/exercises/exercise1.js
@@ -171,13 +171,30 @@ const {from, concat, pipe,zip , of} = require("rxjs");
  fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
     .pipe(
         mergeAll(),
+        mergeMap( movie =>
+                    from(movie.genres).
+                    pipe(map(genre=>
+                                {
+                                return {genre: genre, title: movie.title , directors: movie.directors}
+                                 }
+                             )
+                         )
+                 ),
+         mergeMap( movie  =>
+            fromHttpRequest(`https://orels-moviedb.herokuapp.com/genres/${movie.genre}`
+            ).
+                pipe(map(genre => {return {directors: movie.directors, genre: genre.name , title: movie.title}}))
+            ),
+        filter(movie => movie.genre == "action"),
+
         mergeMap( movie =>  from(movie.directors).pipe(map(director => {return {director: director, title: movie.title}})) ),
         groupBy(movie => movie.director),
         mergeMap( group => group.pipe(count() , map(cnt => [group.key , cnt]))),
+
         max((x, y) => x[1] > y[1] ? 1 : -1),
         mergeMap( ([directorId , movieCnt]) =>
             fromHttpRequest(`https://orels-moviedb.herokuapp.com/directors/${directorId}`).
             pipe(map(director => {return {director: director.name , count: movieCnt}}))
-        )
+        )  ).subscribe(console.log);
 
-    ).subscribe(console.log);
+

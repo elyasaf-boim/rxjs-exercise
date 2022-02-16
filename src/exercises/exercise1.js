dd5db4c87e40a0eecc87cef46a424ee8c3855064
Aviad-hub <57433539+Aviad-hub@users.noreply.github.com>
2022-02-16 16:37:09 +0200

    finish ex17

--- src/exercises/exercise1.js
+++ src/exercises/exercise1.js
@@ -222,31 +222,60 @@ const {from, concat, pipe, zip , of} = require("rxjs");
 
 
 
-  fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
+//  fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
+//    .pipe(
+//        mergeAll(),
+//        take(100),
+//        mergeMap(movie =>  fromHttpRequest(`https://orels-moviedb.herokuapp.com/ratings`).pipe(
+//                    mergeAll(),
+//                    filter(rating => rating.movie == movie.id),
+//                    mergeMap(rating => from(movie.directors).pipe(
+//                            map(director => {
+//                                return {title: movie.title, score: rating.score, director: director}
+//                            }
+//                            )
+//
+//                        )
+//
+//
+//                    )
+//                 )
+//        ),
+//         groupBy(movie => movie.director),
+//        mergeMap(group => zip(of(group.key) , group.pipe(count()) ,group.pipe(reduce((acc, val) => acc + val.score,0)))),
+//        map(tuple => { return {director: tuple[0] , review: tuple[2] / tuple[1] }}),
+//        mergeMap( director_review =>
+//            fromHttpRequest(`https://orels-moviedb.herokuapp.com/directors/${director_review.director}`).
+//            pipe(map(director => {return {director: director.name , avgReview: director_review.review}})) ),
+//        max( (x,y) => x[1] > y[1] ? 1 : -1)
+//                 ).subscribe(console.log);
+
+  fromHttpRequest('https://orels-moviedb.herokuapp.com/directors')
     .pipe(
         mergeAll(),
-        take(100),
-        mergeMap(movie =>  fromHttpRequest(`https://orels-moviedb.herokuapp.com/ratings`).pipe(
+        take(2),
+        mergeMap(director =>  fromHttpRequest(`https://orels-moviedb.herokuapp.com/genres`
+             ).pipe(
                     mergeAll(),
-                    filter(rating => rating.movie == movie.id),
-                    mergeMap(rating => from(movie.directors).pipe(
-                            map(director => {
-                                return {title: movie.title, score: rating.score, director: director}
-                            }
+                    mergeMap(genre => fromHttpRequest(`https://orels-moviedb.herokuapp.com/movies`
+                    ).
+                            pipe(
+                                mergeAll(),
+                                filter(movie => movie.directors.includes(director.id) && movie.genres.includes(genre.id)),
+                                count(),
+                                map(cnt => {return {director: director.id, genre: genre.id, count: cnt}}))
                             )
-
-                        )
-
-
-                    )
-                 )
-        ),
-         groupBy(movie => movie.director),
-        mergeMap(group => zip(of(group.key) , group.pipe(count()) ,group.pipe(reduce((acc, val) => acc + val.score,0)))),
-        map(tuple => { return {director: tuple[0] , review: tuple[2] / tuple[1] }}),
-        mergeMap( director_review =>
-            fromHttpRequest(`https://orels-moviedb.herokuapp.com/directors/${director_review.director}`).
-            pipe(map(director => {return {director: director.name , avgReview: director_review.review}})) ),
-        max( (x,y) => x[1] > y[1] ? 1 : -1)
-                 ).subscribe(console.log);
+                            )
+                    ),
+                    mergeMap(obj =>
+                        fromHttpRequest(`https://orels-moviedb.herokuapp.com/genres/${obj.genre}`
+                                                              ).pipe(
+                                                              mergeMap(genre => fromHttpRequest(`https://orels-moviedb.herokuapp.com/directors/${obj.director}`
+                                                                     ).pipe(map(director => {return {director: director.name, genre: genre.name, count: obj.count}}))
+                                                                  )
+                                                              )
+                    ),
+         groupBy(obj => obj.director , obj => { return { genre: obj.genre, count: obj.count} }),
+         mergeMap(group => zip(of(group.key), group.pipe(toArray()) ))
+        ).subscribe(console.log);
 
